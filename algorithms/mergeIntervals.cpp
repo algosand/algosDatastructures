dc59150f46e8b5810e8f2f3517dfbaca4746a6a6
therealchainman <toddey65@gmail.com>
2021-09-17 23:57:20 -0500

    improved merge intervals algorithm

--- algorithms/mergeIntervals.cpp
+++ algorithms/mergeIntervals.cpp
@@ -5,18 +5,20 @@ The classic merge intervals algorithm that comes in handy often.
 O(n) it uses a sliding window algorithm.  That just checks for overlap with the start and the previous end value.
 
 */
+void setmax(int &a, int b) {
+    a = max(a,b);
+}
 vector<vector<int>> merge(vector<vector<int>>& intervals) {
     sort(intervals.begin(), intervals.end());
     vector<vector<int>> merged;
-    int prevEnd = -1;
-    for (auto &interval : intervals) {
-        int start = interval[0], end = interval[1];
-        if (start>prevEnd) {
+    int n = intervals.size();
+    for (int i = 0;i<n;i++) {
+        int start = intervals[i][0], end = intervals[i][1];
+        if (i==0 || start>merged.back()[1]) {
             merged.push_back({start,end});
         } else {
-            merged.back()[1] = max(prevEnd, end);
+            setmax(merged.back()[1], end);
         }
-        prevEnd = merged.back()[1];
     }
     return merged;
 }
